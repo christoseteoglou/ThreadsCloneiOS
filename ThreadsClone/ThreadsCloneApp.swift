5aeb540536e3cb932a34388d482a5b8243f582d5
christoseteoglou <itsmeglue@gmail.com>
2023-10-24 13:05:25 +0200

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Christos Eteoglou on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
